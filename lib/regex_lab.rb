b84a547de874f5916629f6e2b08c520615f18c19
David Little <littledavid175@gmail.com>
2019-07-21 20:06:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,6 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   if text.start_with?(/un/) && text.end_with?(/ing/) == true
     word << text.start_with?(/un/) && text.end_with?(/ing/)
   end
+  word
 end
 
 def words_five_letters_long(text)
