3e6049b90374914282ad77f119f495105cf4fda8
David Little <littledavid175@gmail.com>
2019-07-21 20:05:44 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,8 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   word = []
-text.start_with?(/un/) && text.end_with?(/ing/)
-if 
+  if text.start_with?(/un/) && text.end_with?(/ing/)
 end
 
 def words_five_letters_long(text)
