581725966e06808b024e142ff4b9d49ade3dfbb3
David Little <littledavid175@gmail.com>
2019-07-21 21:05:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   binding.pry
-text.match(/^[A-Z].+[.!?]/) ? true : false
+text.match(/^[A-Z].+[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
