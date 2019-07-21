066101fafaf503b968cb8e36da0e674a4fa42a8a
David Little <littledavid175@gmail.com>
2019-07-21 20:13:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   word = []
-  text.split(" ")
+  text.split!(" ")
   if text.start_with?("un") && text.end_with?("ing") == true
     word << text.start_with?("un") && text.end_with?("ing")
   end
