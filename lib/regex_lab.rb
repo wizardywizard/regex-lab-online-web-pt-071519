b9cfc97fc227add691d51985ec482b7c2825ecd2
David Little <littledavid175@gmail.com>
2019-07-21 21:32:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+word.start_with?(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = []
+  text = text.split(" ")
+  text.each do |word|
+  if word.start_with?("un") && word.end_with?("ing") == true
+    words << word
+   end
+  end
+  words
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/^[A-Z].+[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/) ? true : false
 end
